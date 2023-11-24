2d45c582f2df9875a36b579ec719ac1f0a2c7a3f
balpreet.balli <Balpreet.Singh@triosstudent.com>
2023-11-24 11:17:08 -0500

    first commit

---
+++ Program.fs
@@ -0,0 +1,4 @@
+﻿type Cuisine = Korean | Turkish
+type MovieType = Regular | IMAX | DBOX | RegularSnacks | IMAXSnacks | DBOXSnacks
+type Activity =
+    | Game | Chill | Movie of MovieType | EatOut of Cuisine | Drive of float * float

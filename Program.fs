8cab2e7ea504c0defe9ca8b3cc7159dc4171cad9
balpreet.balli <Balpreet.Singh@triosstudent.com>
2023-11-24 11:20:53 -0500

    Final commit:adding printfn

--- Program.fs
+++ Program.fs
@@ -20,3 +20,8 @@ let rec budget activity =
 let movieNight = Movie IMAXSnacks
 let dinner = EatOut Korean
 let roadTrip = Drive (100.0, 0.1)
+
+printfn "Movie Night Cost: %f CAD" (budget movieNight)
+printfn "Dinner Cost: %f CAD" (budget dinner)
+printfn "Road Trip Cost: %f CAD" (budget roadTrip)
+
